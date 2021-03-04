eb71a711a26d09931b8237b37a519a60483c678d
francoqueirolo <fgqueirolo@gmail.com>
2021-03-04 12:50:34 -0500

    feat: adding unit test example

--- src/app/pages/about/about.component.spec.ts
+++ src/app/pages/about/about.component.spec.ts
@@ -1,17 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AboutComponent } from './about.component';
+
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({declarations: [AboutComponent]}).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutComponent);
@@ -20,6 +17,19 @@ describe('AboutComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
+  });
+
+  it(`should have as title 'nisum-challenge'`, () => {
+    const fixture = TestBed.createComponent(AboutComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('nisum-challenge');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AboutComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('nisum-challenge app is running!');
   });
 });
